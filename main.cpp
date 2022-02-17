5e90e89619cf106d498ac0f3d5add330a52a1588
CaliDobie <carel009@ucr.edu>
2022-02-16 20:06:54 -0800

    4th commit

--- main.cpp
+++ main.cpp
@@ -4,13 +4,37 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter your name: ";
     string name;
-    cin >> name;
+    int health;
+    int EP;
+    int strength;
+    int magicPower;
+    int physicalDefense;
+    int magicDefense;
+    int speed;
+    string class;
+    string alignment;
     
-    cout << "Select your player - (1)Mage, (2)Warrior, (3)Knight: "
+    Character* user = new Player;
+    cout << "Enter your name: ";
+    cin >> name;
+    user->setName(name);
+    
+    cout << "Select your class - (1)Mage, (2)Warrior, (3)Knight: "
     int choice;
     cin >> choice;
-    
+    switch(choice)
+    {
+        case 1:
+	    //mage
+	    break;
+        case 2:
+	    //warrior
+	    break;
+        case 3:
+	    //knight
+	    break;
+    }
+
     return 0;
 }
