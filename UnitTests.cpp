73242c72e9e8ceb5396303bd365f31707ba0f80c
Isaac Castro <icast065@ucr.edu>
2022-03-13 23:53:17 -0700

    *Unit Tests

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -3,20 +3,41 @@
 //
 #include "spells/Spells.hpp"
 #include "spells/Level1MageSpell.hpp"
+#include "spells/Level1WarriorSpell.hpp"
+#include "spells/Level1KnightSpells.hpp"
 #include "character.hpp"
 #include "player.hpp"
 #include "foe.hpp"
 #include "gtest/gtest.h"
 using namespace std;
-TEST(SpellTests, damage) {
+
+TEST(SpellTests, MageDamage) {
     Character* testChar = new Foe;
     testChar->setHealth(10);
     Spells* testSpell = new Level1MageSpell;
     testSpell->castSpell(testChar);
-    cout << testChar->getHealth();
     int testHealth = testChar->getHealth();
     EXPECT_EQ(-10, testHealth);
 }
+
+TEST(SpellTests, WarriorDamage) {
+    Character* testChar = new Foe;
+    testChar->setHealth(10);
+    Spells* testSpell = new Level1WarriorSpell;
+    testSpell->castSpell(testChar);
+    int testHealth =  testChar->getHealth();
+    EXPECT_EQ(-30, testHealth);
+}
+
+TEST(SpellTests, KnightDamage) {
+    Character* testChar = new Foe;
+    testChar->setHealth(10);
+    Spells* testSpell = new Level1KnightSpells;
+    testSpell->castSpell(testChar);
+    int testHealth = testChar->getHealth();
+    EXPECT_EQ(-20, testHealth);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
