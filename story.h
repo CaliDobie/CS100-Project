aa8be7b697c02f30fc8907890637a3fff9f96df7
srsmith516 <s.r.smith427@gmail.com>
2022-03-15 11:45:30 -0700

    Init story functions

---
+++ story.h
@@ -0,0 +1,15 @@
+//Story-Driven Functions
+//     How to use combat system
+//          vector<Character*> participants;
+//              participants.push_back(foe_1_npc);
+//                  participants.push_back(foe_2_npc);
+//                      participants.push_back(foe_3_npc);
+//                          participants.push_back(foe_4_npc);
+//                              participants.push_back(foe_5_npc);
+//
+//                                  Scene* testCombat = new Combat(participants, user, playerSpells); <-- use this constructor
+//                                      testCombat->SetUpScene(); <-- use this start combat scene
+//
+//
+//
+//
