e8ce49ecb0dd8400b2f4f4196cd88db6d9af1f32
srsmith516 <s.r.smith427@gmail.com>
2022-03-15 13:33:44 -0700

    Created modules for story upkeep

--- story.cpp
+++ story.cpp
@@ -2,13 +2,24 @@
 
 ing namespace std;
    story:: story() {rumor = 0;trial1 = false; trial2 = false; trial3 = false; }
-
-    int get_rumor(){
+    void story:set_rumor(int loc_rumor){ rumor = loc_rumor;}
+    int story::get_rumor(){
 	return rumor;	}
-    bool get_trial1(){ return trial1;}
-    bool get_trial(){return trial2; }
-    void set_trial(bool pass, int input){ if(input ==1){trial1 = true;}
+    bool story::get_trial1(){ return trial1;}
+    bool story::get_trial(){return trial2; }
+    void story::set_trial(bool pass, int input){
+if(pass == true){
+ if( input ==1){trial1 = true;}
 					else if(input == 2){trial2 = true;}
 					else {trial3 = true;}
 	}
-    void set_rumor(int input){rumor =  input;}
+
+	else if(pass == false){
+		if(input == 1){ trial1 = false; }
+		else if(input == 2) { trial2  = false;}
+		else if(input == 3) { trial3 = false;}
+		else{ rumor = 0; }
+	}
+	else{rumor = 0;}
+}
+    void story::set_rumor(int input){rumor =  input;}
